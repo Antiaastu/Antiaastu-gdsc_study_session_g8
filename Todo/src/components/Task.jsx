93828338d79ffb95b8097c924236532c6c5706af
Antiaastu <136442178+Antiaastu@users.noreply.github.com>
2023-12-20 14:02:50 +0300

    Todo

--- Todo/src/components/Task.jsx
+++ Todo/src/components/Task.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 const Task = ({ task, onDelete, onComplete }) => {
   return (
     <div className={`task ${task.completed ? 'completed' : ''}`}>
-      <p>{task.text}</p>
-      <button onClick={() => onDelete(task.id)}>Delete</button>
-      <button onClick={() => onComplete(task.id)}>
-        {task.completed ? 'Undo' : 'Complete'}
+       <button  onClick={() => onComplete(task.id)}>
+        {task.completed ? 'Undo' : 'Done'}
       </button>
+      <p>{task.text}</p>
+      <button onClick={() => onDelete(task.id)}>X</button>
+     
     </div>
   );
 };
